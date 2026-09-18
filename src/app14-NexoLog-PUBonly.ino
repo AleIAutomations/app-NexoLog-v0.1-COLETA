3d221a9802fa81a0d372af734bde47c18fe7cf0a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-17 22:12:30 -0300

    ok2

--- src/app14-NexoLog-PUBonly.ino
+++ src/app14-NexoLog-PUBonly.ino
@@ -2,6 +2,10 @@
 #include "ESP32SensorsAccel.hpp"
 #include "ESP32SensorsAmbiente.hpp"
 #include "ESP32SensorsDistancia.hpp"
+#include <Wifi.h>
+#include <WifiClient.h>
+#include <PubSubClient.h>
+#include <Arduinojson.h>
 
 #define DHT_PIN 4
 #define DHT_MODEL DHT22
@@ -10,15 +14,47 @@
 #define SCL_PIN 23
 #define SDA_PIN 22
 
+// credenciais WIFI
+const char" WIFI-SSID = "Wokwi-GUEST";
+const char" WIFI-PASSWORD = "";
+
+// Conexao MQTT
+#define MQTT_SERVER "host.wokwi.internal"
+#define MQTT_PORT 1883
+#define MQTT_CLIENT_ID "Ale"
+#define MQTT_PUB_TOPIC "FIAPIoT/nexolog/equipe01/dados"
+
+WifiClient wifiClient;
+PubSubClient mqttClient(wifiCliente);
+
+const uint32_t INTERVALO_RECONEXAO = 5000;
+uint32_t ultimatentativaWifi = 0;
+uint32_t ultimaTentativaMQTT
+
 //Intervalo de coleta
 const uint32_t INTERVALO_COLETA = 2500;
 uint32_t tempoAnterior = 0;
+
+void conectarwifi() {
+  Wifi.mode(WIFI_STA);
+  Wifi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Wifi.serSleep(false);
+}
+
+void conectarMQTT() {
+  if (mqttClient.connect(MQTT_CLIENT_ID))
+  Serial.printin("[MQTT]")
+}
 
 void setup() {
   Serial.begin(115200);
   ESP32Sensors::Ambiente::inicializar(DHT_PIN, DHT_MODEL);
   ESP32Sensors::Distancia::inicializar(TRIG_PIN, ECHO_PIN);
   ESP32Sensors::Accel::inicializar(SCL_PIN, SDA_PIN);
+
+  conectarWifi();
+  mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+
 
   Serial.println("temp,umid,dist,movimentacao");
 }
